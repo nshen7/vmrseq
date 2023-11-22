3ef114ed2a319dd021641d0b4482c2a05bf723f1
NingShen1997 <shenning1997@gmail.com>
2023-11-22 13:11:31 -0800

    fixing bugs in vignette

--- R/HDF5NAdrop2matrix.R
+++ R/HDF5NAdrop2matrix.R
@@ -1,14 +1,13 @@
-#' Coerce a NA-dropped HDF5Matrix back to regular matrix form where NAs are not dropped to 0.
+#' Coerce a NA-dropped HDF5 matrix back to regular matrix form where NAs are not dropped to 0.
 #'
-#' @param HDF5Matrix an HDF5Matrix (e.g. assay output from vmrseq::data.pool)
-#' with dropped NA values
+#' @param hdf5_assay an DelayedMatrix object with dropped NA values (e.g. an assay from HDF5SummarizedExpriment object saved by vmrseq::data.pool)
 #'
 #' @importFrom recommenderlab dropNA2matrix
 #' @return
 #' @export
 #'
 #' @examples
-HDF5NAdrop2matrix <- function(hdf5_matrix) {
-  mat <- hdf5_matrix %>% as("sparseMatrix") %>% recommenderlab::dropNA2matrix()
+HDF5NAdrop2matrix <- function(hdf5_assay) {
+  mat <- hdf5_assay |> as("matrix") |> as("sparseMatrix") |> recommenderlab::dropNA2matrix()
   return(mat)
 }
